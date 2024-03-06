fcd43615e5573abc97fe0d5a02e8660e54797d2a
Sergio <qjow.xd@gmail.com>
2024-03-06 01:23:02 -0300

    read.md added

--- Transfeera - Notebooks/ETL/dim_contratos.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC   MERGE INTO dw.dim_contratos as dim_c USING staging.contratos as stg_c ON dim_c.id_contrato = stg_c.id_contrato
-# MAGIC   WHEN NOT MATCHED THEN
-# MAGIC INSERT
-# MAGIC   *;
-# MAGIC -- WHEN MATCHED THEN
-# MAGIC   -- TODO
--- Transfeera - Notebooks/ETL/staging: limpeza.py
+++
@@ -1,3 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC DROP SCHEMA IF EXISTS staging CASCADE;
